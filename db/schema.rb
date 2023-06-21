3ea564b6c24af3cc63fc2fa612326effaabedb24
Matthew Hutchinson <matt@hiddenloop.com>
2023-06-21 15:51:42 +0100

    add highscore scaffold for more testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_06_21_143710) do
+ActiveRecord::Schema[7.1].define(version: 2023_06_21_144604) do
+  create_table "high_scores", force: :cascade do |t|
+    t.string "game"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
